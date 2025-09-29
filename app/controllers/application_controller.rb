d36e0835e04a90e4d2023eceb5249037d631f91a
Anouar EL KHALFI <147706023+Anouar-Elkhalfi@users.noreply.github.com>
2025-09-29 11:26:01 +0200

    teste

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,7 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
 
   after_action :verify_authorized, except: :index, unless: :skip_pundit?
-after_action :verify_policy_scoped, only: :index, unless: :skip_pundit?
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  after_action :verify_policy_scoped, if: :verify_policy_scoped_needed?
 
   protected
 
